23934fbc0d3f067e5cb8277a522ff4198e92c0fb
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-06-11 10:02:19 +0200

    Styled the HeroComponent

--- src/components/HeroContainer.tsx
+++ src/components/HeroContainer.tsx
@@ -3,7 +3,11 @@ import React from "react";
 import { HeroContainerProps } from "../interfaces/components";
 
 const HeroContainer: React.FC<HeroContainerProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <section className="w-full h-dvh flex justify-center items-center">
+      <div className="w-full max-w-screen-2xl">{children}</div>
+    </section>
+  );
 };
 
 export default HeroContainer;
