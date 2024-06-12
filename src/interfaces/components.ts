2f4582939d8625c39fe9da36f241c5eb139354a4
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-06-12 08:37:22 +0200

    Removed unecessary declarations

--- src/interfaces/components.ts
+++ src/interfaces/components.ts
@@ -8,9 +8,6 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   primary?: boolean;
   secondary?: boolean;
-  children: React.ReactNode;
-  disabled?: boolean;
-  className?: string;
 }
 
 export interface ContainerProps {
