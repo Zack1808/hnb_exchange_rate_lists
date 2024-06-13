b8c46635618299064a3b1332732f2a3d22b930eb
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2024-06-13 22:28:44 +0200

    Added responsivenes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
+import { ImMenu } from "react-icons/im";
+import { FaXmark } from "react-icons/fa6";
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const toggleOpen = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
-    <nav className="p-3 shadow-lg flex justify-center items-center fixed top-0 w-full z-50 bg-white">
+    <nav className="md:py-3 py-5 px-3 shadow-lg flex justify-center items-center fixed top-0 w-full z-40 bg-white">
       <div className="w-full md:max-w-screen-2xl flex justify-between items-center">
         <Link to="/" className="flex gap-1 items-center justify-center">
           <img src="/favicon.svg" className="w-10 md:w-14" />
@@ -11,7 +19,15 @@ const Navbar: React.FC = () => {
             HNB
           </h2>
         </Link>
-        <div>
+
+        {/* Big screen */}
+        <div className="md:flex hidden">
+          <Link
+            to="/"
+            className="text-lg transition hover:text-red-600 hover:bg-gray-50 p-3"
+          >
+            Početna
+          </Link>
           <Link
             to="/tecaj"
             className="text-lg transition hover:text-red-600 hover:bg-gray-50 p-3"
@@ -25,6 +41,60 @@ const Navbar: React.FC = () => {
             Povijest tečaja za USD
           </Link>
         </div>
+
+        {/* Small screen */}
+        <ImMenu
+          className="md:hidden flex text-4xl text-red-600"
+          onClick={toggleOpen}
+        />
+
+        {isOpen && (
+          <div className="absolute top-0 left-0 w-full h-dvh bg-black/20 md:hidden">
+            <div
+              className={`bg-white absolute left-0 top-0 bottom-0 px-8 py-5 flex flex-col gap-10 items-end scale-x-0 origin-left ${
+                isOpen && "animate-menu-open"
+              }`}
+            >
+              <div
+                className={`flex justify-between w-full opacity-0 ${
+                  isOpen && "animate-menu-links-open"
+                }`}
+              >
+                <Link
+                  onClick={toggleOpen}
+                  to="/"
+                  className="flex gap-0 items-center justify-center "
+                >
+                  <img src="/favicon.svg" className="w-7 md:w-14" />
+                  <h2 className="font-bold text-red-600 text-2xl flex ">HNB</h2>
+                </Link>
+                <FaXmark
+                  className="text-4xl text-red-600"
+                  onClick={toggleOpen}
+                />
+              </div>
+              <div
+                className={`flex flex-col gap-2 opacity-0 ${
+                  isOpen && "animate-menu-links-open"
+                }`}
+              >
+                <Link onClick={toggleOpen} to="/" className="text-lg py-3">
+                  Početna
+                </Link>
+                <Link onClick={toggleOpen} to="/tecaj" className="text-lg py-3">
+                  Pregledaj tečajeve
+                </Link>
+                <Link
+                  onClick={toggleOpen}
+                  to="/povijest/USD"
+                  className="text-lg py-3"
+                >
+                  Povijest tečaja za USD
+                </Link>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,19 @@ export default {
           "0%": { rotate: "0deg" },
           "100%": { rotate: "361deg" },
         },
+        "menu-open": {
+          "0%": { transform: "scaleX(0)" },
+          "100%": { transform: "scaleX(1)" },
+        },
+        "menu-links-open": {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
       },
       animation: {
         rotation: "rotation 1s linear infinite",
+        "menu-open": "menu-open .2s ease-in-out .05s forwards",
+        "menu-links-open": "menu-links-open .2s ease-in-out .25s forwards",
       },
     },
   },
