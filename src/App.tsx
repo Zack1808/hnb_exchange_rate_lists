79efccc8dd269448d2a5cd4721519fa88de926df
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2025-10-25 18:09:38 +0200

    feat(routing): set up React Router in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import ExchangeRate from "./pages/ExchangeRate";
+import ExchangeHistory from "./pages/ExchangeHistory";
+import PageNotFound from "./pages/PageNotFound";
 
 const App: React.FC = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tecaj" element={<ExchangeRate />} />
+          <Route path="/povijest" element={<ExchangeHistory />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;
