67c2d1ca9b9de9705b208c430651c03ab82c7d4d
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2025-10-25 18:01:34 +0200

    chore: initialize project structure with clean src directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { PageNotFound, ExchangeRatePage, HistoryRatePage, Home } from "./pages";
-
-import { Navbar, Footer } from "./components";
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tecaj" element={<ExchangeRatePage />} />
-        <Route path="/povijest/:currency" element={<HistoryRatePage />} />
-        <Route path="/povijest/:currency/:date" element={<HistoryRatePage />} />
-
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+  return <div>App</div>;
 };
 
 export default App;
