bc3ae221c1b4f05e295f5054b3a79651d005e9ba
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2025-10-26 12:40:58 +0100

    feat(hero): add background image and implement styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,9 @@ import Hero from "../components/common/Hero";
 const Home: React.FC = React.memo(() => {
   return (
     <>
-      <Hero>Home</Hero>
+      <Hero className="relative overflow-hidden before:absolute before:inset-[-500px] before:bg-[url(/bg.png)] before:bg-[length:600px_500px] before:bg-[position:10px_20px] before:z-[-10] before:-rotate-45 bg-gradient-to-br from-transparent to-white to-65%">
+        Home
+      </Hero>
     </>
   );
 });
