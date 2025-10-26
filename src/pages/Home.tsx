876422796fb6605e66e7d4567792a02a37f61824
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2025-10-26 12:47:10 +0100

    feat(components): create Button component and integrate with Hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
 import Hero from "../components/common/Hero";
+import Button from "../components/common/Button";
 
 const Home: React.FC = React.memo(() => {
   return (
     <>
       <Hero className="relative overflow-hidden before:absolute before:inset-[-500px] before:bg-[url(/bg.png)] before:bg-[length:600px_500px] before:bg-[position:10px_20px] before:z-[-10] before:-rotate-45 bg-gradient-to-br from-transparent to-white to-65%">
-        <div className="grid gap-4 flex-1 border">
+        <div className="grid gap-4 xl:flex-4/12">
           <h1 className="text-5xl md:text-6xl font-semibold text-gray-800">
             Provjera tečaja u stvarnom vremenu
           </h1>
@@ -14,9 +15,11 @@ const Home: React.FC = React.memo(() => {
             Svi službeni tečajevi Hrvatske narodne banke na jednom mjestu
           </p>
 
-          {/* TODO - build Button component and place here */}
+          <Button variant="primary" className="mt-6">
+            Saznajte više
+          </Button>
         </div>
-        <div className="hidden relative xl:flex flex-1 border h-[400px]">
+        <div className="hidden relative xl:flex flex-1 h-[400px]">
           {/* TODO - build chart component and display growth/decline of the middle rate since 1.1.2023 and growth/decline of the middle rate depending on the rate of the previous day for a randomly selected currency */}
         </div>
       </Hero>
