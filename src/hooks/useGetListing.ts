3622fae2c9c86ec992b4d94b347cbbeb3bdee460
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2025-10-27 12:28:48 +0100

    feat(hooks): create data fetching hook with mock implementation

--- src/hooks/useGetListing.ts
+++ src/hooks/useGetListing.ts
@@ -1,3 +1,49 @@
-export const useGetListings = () => {
-  return { data: null, loading: false, error: null };
+import { useState, useCallback } from "react";
+
+import {
+  mockExchangeRateHistoryList,
+  MOCK_CONFIG,
+} from "../services/mock/mockData";
+
+type GetCurrencyHistoryProps = (
+  fromDate: string,
+  toDate: string
+) => Promise<Record<string, string>[] | undefined>;
+
+interface UseGetListingReturnProps {
+  loading: boolean;
+  error: string | null;
+  getCurrencyHistory: GetCurrencyHistoryProps;
+}
+
+type UseGetListingProps = (useMockData: boolean) => UseGetListingReturnProps;
+
+export const useGetListings: UseGetListingProps = (
+  useMockData = MOCK_CONFIG.enableMockData
+) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const getCurrencyHistory: GetCurrencyHistoryProps = useCallback(
+    async (fromDate, toDate) => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        if (useMockData) {
+          await new Promise((resolve) =>
+            setTimeout(resolve, MOCK_CONFIG.apiDelay)
+          );
+
+          return mockExchangeRateHistoryList;
+        }
+      } catch (err: any) {
+      } finally {
+        setLoading(false);
+      }
+    },
+    [useMockData]
+  );
+
+  return { loading, error, getCurrencyHistory };
 };
