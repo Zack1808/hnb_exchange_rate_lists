2dc1445ead9ea89ab141bdba74c667acdaf77eb0
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2025-10-27 13:29:29 +0100

    fix(routing): scroll to top on page navigation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -64,6 +64,10 @@ const Home: React.FC = React.memo(() => {
     };
   }, [fetchData]);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <Hero className="relative overflow-hidden before:absolute before:inset-[-500px] before:bg-[url(/bg.png)] before:bg-[length:600px_500px] before:bg-[position:10px_20px] before:z-[-10] before:-rotate-45 bg-gradient-to-br from-transparent to-white to-65%">
