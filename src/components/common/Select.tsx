472c66589f56fd36ecf78e3812d1db55c2c597b5
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2026-07-30 20:20:47 +0200

    refactor(select): optimize component performance

--- src/components/common/Select.tsx
+++ src/components/common/Select.tsx
@@ -1,11 +1,17 @@
-import React, { useState, useRef, useCallback, useEffect } from "react";
+import React, {
+  useState,
+  useRef,
+  useCallback,
+  useEffect,
+  useMemo,
+} from "react";
 import { FaCaretUp, FaCaretDown, FaX } from "react-icons/fa6";
 
 import { useOutsideClick } from "../../hooks/useOutsideClick";
 
 import Button from "./Button";
 
-type Options<T = string> = { value: T; label: string; disabled?: boolean };
+type Options = { value: string; label: string };
 
 interface SingleSelectProps {
   value: string | null;
@@ -17,7 +23,7 @@ interface MultiSelectProps {
 }
 
 type SelectProps = {
-  options: Options<string>[];
+  options: Options[];
   onChange?: (value: string) => void;
   placeholder?: string;
   disabled?: boolean;
@@ -35,7 +41,7 @@ const Select: React.FC<SelectProps> = ({
   id,
 }) => {
   const [selectOpen, setSelectOpen] = useState<boolean>(false);
-  const [hasSpaceBellow, setHasSpaceBellow] = useState<boolean>(true);
+  const [hasSpaceBelow, setHasSpaceBelow] = useState<boolean>(true);
   const [highlightedIndex, setHighlightedIndex] = useState<number | null>(null);
 
   const selectRef = useRef<HTMLDivElement>(null);
@@ -43,10 +49,51 @@ const Select: React.FC<SelectProps> = ({
   const optionsRef = useRef<(HTMLButtonElement | null)[]>([]);
   const optionRef = useRef<HTMLDivElement>(null);
 
+  const optionsMap = useMemo(() => {
+    return new Map(options.map((o) => [o.value, o]));
+  }, [options]);
+
+  const displaySelectValue = useMemo(() => {
+    if (multiple && value?.length) {
+      return (value as string[]).map((item) => (
+        <Button
+          variant="primary"
+          className="border-none !px-2 !py-0.5 text-sm"
+          type="button"
+          key={item}
+          onClick={(e: React.MouseEvent) => {
+            e.stopPropagation();
+            onChange?.(item);
+          }}
+        >
+          {optionsMap.get(item)?.label}
+          <FaX />
+        </Button>
+      ));
+    } else if (!multiple && value) {
+      return optionsMap.get(value)?.label;
+    } else return placeholder ?? "Select...";
+  }, [multiple, value, placeholder, onChange, optionsMap]);
+
+  const checkSpace = useCallback(() => {
+    if (!divRef.current || !optionRef.current) return;
+
+    const select = divRef.current.getBoundingClientRect();
+    const options = optionRef.current.offsetHeight;
+
+    const availableSpace = window.innerHeight - select.bottom;
+
+    setHasSpaceBelow(options < availableSpace);
+  }, []);
+
   const toggleSelect = useCallback(() => {
-    setSelectOpen((prevState) => !prevState);
-    setHighlightedIndex(null);
-    checkSpace();
+    setSelectOpen((prev) => {
+      if (prev) {
+        setHighlightedIndex(null);
+      }
+
+      return !prev;
+    });
   }, []);
 
   const handleKeyPress = useCallback(
@@ -60,27 +107,12 @@ const Select: React.FC<SelectProps> = ({
             checkSpace();
             setHighlightedIndex(0);
           } else {
-            onChange?.(options[highlightedIndex as number].value);
+            if (highlightedIndex === null) return;
+            onChange?.(options[highlightedIndex].value);
             !multiple && toggleSelect();
           }
           break;
         case "ArrowUp":
-          event.preventDefault();
-          if (!options.length) break;
-          if (!selectOpen) {
-            setSelectOpen(true);
-            setHighlightedIndex(0);
-            break;
-          }
-          const arrowUp = (highlightedIndex as number) - 1;
-          if (arrowUp >= 0) {
-            setHighlightedIndex(arrowUp);
-            optionsRef.current[arrowUp]?.scrollIntoView({
-              behavior: "smooth",
-              block: "nearest",
-            });
-          }
-          break;
         case "ArrowDown":
           event.preventDefault();
           if (!options.length) break;
@@ -89,10 +121,14 @@ const Select: React.FC<SelectProps> = ({
             setHighlightedIndex(0);
             break;
           }
-          const arrowDown = (highlightedIndex as number) + 1;
-          if (arrowDown < options.length) {
-            setHighlightedIndex(arrowDown);
-            optionsRef.current[arrowDown]?.scrollIntoView({
+          if (highlightedIndex === null) return;
+          const highlight =
+            event.key === "ArrowDown"
+              ? highlightedIndex + 1
+              : highlightedIndex - 1;
+          if (highlight >= 0 && highlight < options.length) {
+            setHighlightedIndex(highlight);
+            optionsRef.current[highlight]?.scrollIntoView({
               behavior: "smooth",
               block: "nearest",
             });
@@ -104,33 +140,20 @@ const Select: React.FC<SelectProps> = ({
           setHighlightedIndex(null);
       }
     },
-    [value, onChange, selectOpen, highlightedIndex, highlightedIndex],
+    [onChange, selectOpen, multiple, options, toggleSelect, highlightedIndex],
   );
 
   const selectItem = useCallback(
     (item: string) => {
-      onChange && onChange(item);
+      onChange?.(item);
       !multiple && setSelectOpen(false);
     },
     [onChange, multiple],
   );
 
-  const checkSpace = useCallback(() => {
-    if (!divRef.current || !optionRef.current) return;
-
-    const select = divRef.current.getBoundingClientRect();
-    const options = optionRef.current.offsetHeight;
-
-    const availableSpace = window.innerHeight - select.bottom;
-
-    setHasSpaceBellow(options < availableSpace);
-  }, [selectOpen, divRef.current, optionRef.current]);
-
   useOutsideClick(selectRef, () => {
-    (() => {
-      setSelectOpen(false);
-      setHighlightedIndex(null);
-    })();
+    setSelectOpen(false);
+    setHighlightedIndex(null);
   });
 
   useEffect(() => {
@@ -143,7 +166,7 @@ const Select: React.FC<SelectProps> = ({
     return () => {
       window.removeEventListener("scroll", checkSpace, true);
     };
-  }, [selectOpen]);
+  }, [selectOpen, checkSpace]);
 
   return (
     <div ref={selectRef} className="w-full relative">
@@ -156,32 +179,12 @@ const Select: React.FC<SelectProps> = ({
         className={`flex gap-4 items-center justify-between w-full p-2 border outline-none border-gray-300 bg-white rounded-sm ${!disabled ? "focus-within:ring-1 ring-red-300" : ""} `}
       >
         <span className="w-full border-r-1 border-gray-400 flex gap-1 flex-wrap ">
-          {multiple && value?.length
-            ? (value as string[]).map((item, index) => (
-                <Button
-                  variant="primary"
-                  className="border-none !px-2 !py-0.5 text-sm"
-                  type="button"
-                  key={index}
-                  onClick={(e: React.MouseEvent) => {
-                    e.stopPropagation();
-                    onChange?.(item);
-                  }}
-                >
-                  {options.find((o) => o.value === item)?.label}
-                  <FaX />
-                </Button>
-              ))
-            : !multiple && value
-              ? options.find((o) => o.value === value)?.label
-              : placeholder
-                ? placeholder
-                : "Select..."}
+          {displaySelectValue}
         </span>
         {!selectOpen ? <FaCaretDown /> : <FaCaretUp />}
       </div>
       <div
-        className={`${selectOpen ? "flex flex-col" : "hidden"} fixed md:absolute z-50 w-full ${hasSpaceBellow ? "md:top-full md:mt-1 md:bottom-auto" : "md:bottom-full md:mb-1 md:top-auto"} top-0 bottom-0 md:left-auto left-0 md:right-auto right-0 bg-black/40 shadow-md md:max-h-64 flex items-center justify-center`}
+        className={`${selectOpen ? "flex flex-col" : "hidden"} fixed md:absolute z-50 w-full ${hasSpaceBelow ? "md:top-full md:mt-1 md:bottom-auto" : "md:bottom-full md:mb-1 md:top-auto"} top-0 bottom-0 md:left-auto left-0 md:right-auto right-0 bg-black/40 shadow-md md:max-h-64 flex items-center justify-center`}
         onClick={() => setSelectOpen(false)}
       >
         <div
@@ -189,10 +192,10 @@ const Select: React.FC<SelectProps> = ({
           ref={optionRef}
           onClick={(event: React.MouseEvent) => {
             event.stopPropagation();
-            event.target !== optionRef?.current &&
-              !multiple &&
-              setSelectOpen(false) &&
+            if (event.target !== optionRef.current && !multiple) {
+              setSelectOpen(false);
               setHighlightedIndex(null);
+            }
           }}
         >
           {options.map((item, index: number) => (
