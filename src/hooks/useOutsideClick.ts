209340e93fad5a1617a1797f95d560d1d9f5ccc2
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2026-08-28 23:02:18 +0200

    refactor(outside-click): simplify callback reference handling

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -4,13 +4,13 @@ export const useOutsideClick = (
   ref: React.RefObject<HTMLElement | null>,
   callback: () => void,
 ) => {
+  const callbackRef = useRef(callback);
+
   useEffect(() => {
-    const callbackRef = useRef(callback);
+    callbackRef.current = callback;
+  }, [callback]);
 
-    useEffect(() => {
-      callbackRef.current = callback;
-    }, [callback]);
-
+  useEffect(() => {
     const handleClick = (event: MouseEvent) => {
       const target = event.target;
 
