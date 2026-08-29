b0551deac80000d1314971957adef04a93111a25
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2026-08-29 15:52:13 +0200

    feat(not-found): add localized 404 page

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import Button from "../components/common/Button";
+
 const PageNotFound: React.FC = React.memo(() => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="h-[100dvh] w-full flex items-center justify-center flex-col gap-3">
+      <strong className="text-9xl text-red-600">404</strong>
+      <strong className="text-3xl text-red-600">Stranica nije pronađena</strong>
+      <p className="text-lg text-gray-800 mt-10 mb-5">
+        Provjerite jeste li ispravno unijeli adresu ili se vratite na početnu
+        stranicu HNB-a.
+      </p>
+      <Button to="/" variant="primary">
+        Povratak na početnu
+      </Button>
+    </div>
+  );
 });
 
 export default PageNotFound;
